ba590450d77b991febda5a075c5e99b56f48c67e
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2021-09-04 21:50:06 +0530

    Update settings.py

--- atsystem/settings.py
+++ atsystem/settings.py
@@ -132,6 +132,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'classroom.User'
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default']=dj_database_url.config()
