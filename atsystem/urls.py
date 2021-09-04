3fa044db5e5730f0f41acc828750d8a92f8c76ab
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2021-09-04 20:33:24 +0530

    Update urls.py

--- atsystem/urls.py
+++ atsystem/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import Home
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('classroom/', include('classroom.urls')),
     path('',Home.as_view(), name='home'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
